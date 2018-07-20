0392bb2fe9b3220d95a40e3d603fc121d710d445
gmrald123 <gm.boknat@gmail.com>
2018-07-21 03:46:11 +0800

    setup md bootstrap for vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,34 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <navbar class="indigo" name="Vue Firebase CRUD" href="#/">
+      <navbar-collapse>
+        <navbar-nav>
+          <navbar-item href="#/" v-bind:class="{'active': $route.name == 'home'}">Home</navbar-item>
+          <navbar-item href="#/about" v-bind:class="{'active': $route.name == 'about'}">About</navbar-item>
+        </navbar-nav>
+      </navbar-collapse>
+    </navbar>
     <router-view/>
   </div>
 </template>
+
+<script>
+import Navbar from 'mdbvue/src/components/Navbar.vue';
+import NavbarItem from 'mdbvue/src/components/NavbarItem.vue';
+import NavbarNav from 'mdbvue/src/components/NavbarNav.vue';
+import NavbarCollapse from 'mdbvue/src/components/NavbarCollapse.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Navbar,
+    NavbarItem,
+    NavbarNav,
+    NavbarCollapse
+  }
+};
+
+</script>
 
 <style lang="scss">
 #app {
@@ -14,16 +36,14 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  a, span {
+    color: white;
+  }
+
+  .router-link-active {
+    color: red;
+    background-color: green;
   }
 }
+
 </style>
